e6fe00c7f03beabf1bc580cd0f36bef53a64fdd5
George Tarnaras <george.tarnaras@automationlogic.com>
2020-12-04 16:50:49 +0000

    Various updates

--- .history/setup_20201204164715.sh
+++ .history/setup_20201204164715.sh
@@ -6,7 +6,8 @@ function set_me_up {
     wget http://localhost:8080/jnlpJars/jenkins-cli.jar
     for jenkins_job in ${DISCOVER_DIR}/*.xml; do
         java -jar jenkins-cli.jar create-job $(basename ${jenkins_job} | cut -d"." -f1) < ${jenkins_job}
-        java -jar jenkins-cli.jar update-job $(basename ${jenkins_job} | cut -d"." -f1) < ${jenkins_job}    done
+        java -jar jenkins-cli.jar update-job $(basename ${jenkins_job} | cut -d"." -f1) < ${jenkins_job})
+    done
     rm jenkins-cli.jar
 }
 
